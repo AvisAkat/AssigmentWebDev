91a0ada0d9aab494c8bbf3bccffa66fabe013a9c
AvisAkat <davisakat5@gmail.com>
2024-12-21 01:27:00 +0000

    added mini search box

--- assignment.js
+++ assignment.js
@@ -13,3 +13,19 @@ function sidebar(active) {
     }
     
 }
+
+
+
+// mini search box
+
+function miniSearchBox(active) {
+    let mini = document.querySelector('.mini-searchbox');
+
+    if (active) {
+        mini.style.top = '0';
+    }
+    if (!active) {
+        mini.style.top = '-500px';
+    }
+    
+}
